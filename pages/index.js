ddeeff744cf03682f0539e7e6c6d6f981d5a7387
zaxgit <zwalter95@gmail.com>
2022-12-17 15:20:58 -0600

    created layout, added login and register user form

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,19 @@ import Image from 'next/image';
 
 import MobileNav from '../components/layout/mobile/mobile-nav';
 import Footer from '../components/layout/footer';
+import Header from '../components/layout/header';
 
 export default function Home() {
   // const isMobile = window.matchMedia('max-width: 767px');
 
   return (
-    <div>
+    <>
       <Head>
         <title>Biblioteca</title>
         <meta name='description' content='Your library app' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <header></header>
-      <main></main>
-      <footer>
-        <Footer />
-      </footer>
-    </div>
+      <Layout></Layout>
+    </>
   );
 }
