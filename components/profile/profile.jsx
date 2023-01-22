de32096796922e64b3185bc6da9caff18135bf4d
zaxgit <zwalter95@gmail.com>
2023-01-22 01:32:42 -0600

    read, delete  and display modal added for books.

---
+++ components/profile/profile.jsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Profile() {
+  return (
+    <div className='profile'>
+      <Image className='profile-image' src='#' height='50' width='50' />
+    </div>
+  );
+}
